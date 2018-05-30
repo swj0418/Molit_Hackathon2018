6a9dfd056eff455767a8465469837c402b71ebdf
Sangwon Jeong <swj0418@hotmail.com>
2018-05-30 18:44:03 +0900

    Update BusParserI.java
    Index directory structure

--- Molit_Hackathon2018/src/pro/jeong/molithackathon2018/data/parser/BusParserI.java
+++ Molit_Hackathon2018/src/pro/jeong/molithackathon2018/data/parser/BusParserI.java
@@ -1,8 +1,19 @@
 package pro.jeong.molithackathon2018.data.parser;
 
 public class BusParserI {
-
-    public BusParserI() {
-
+    /**
+    INDEX STRUCTURE
+    ::: Directory System :::
+    ./Index/ <index anchor> / <yy> / <MMdd>/ index.csv
+    ==> <index anchor> ex) BY_TRIP_ID
+    
+    1. BY_TRIP_ID  :  0:TRIPID      1:TARGET FILE ABSOLUTE PATH      2:LINE
+                      STRING        STRING                           STRING
+    */
+    
+    public BusParserI(String indexPath) {
+        
     }
+    
+    
 }
